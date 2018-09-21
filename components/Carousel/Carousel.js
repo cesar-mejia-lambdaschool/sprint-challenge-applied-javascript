55a6ec511cd8820c227a3a0b811a61cbe83a2307
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2018-09-21 11:40:06 -0700

    carousel complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,29 @@
-class Carousel {
+class Carousel {}
 
-}
+let carousel = document.querySelector('.carousel')
+const left = carousel.firstElementChild
+console.log('left', left)
+const right = carousel.lastElementChild
+console.log('right', right)
+const imgs = carousel.querySelectorAll('img')
+console.log('imgs', imgs)
+let index = 0
 
-let carousel = document.querySelector();
+left.addEventListener('click', () => {
+  --index
+  if (index < 0) index = 3
+  imgs.forEach(img => (img.style.display = 'none'))
+  imgs[index].style.display = 'block'
+})
 
+right.addEventListener('click', () => {
+  ++index
+  if (index > 3) index = 0
+  imgs.forEach(img => (img.style.display = 'none'))
+  imgs[index].style.display = 'block'
+})
+
+console.log('index', index)
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
